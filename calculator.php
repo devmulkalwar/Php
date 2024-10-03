99dd474f27c2a6155045f5b13285224f8ea98e5b
devmulkalwar <devmulkalwar95@gmail.com>
2024-10-03 21:57:07 +0530

    initial commit

---
+++ calculator.php
@@ -0,0 +1,11 @@
+<?php
+    echo "Calculator" , "<br/>" ;
+    $a = 10;
+    $b = 8;
+    echo "Value of a : ", $a , "<br/>";
+    echo "Value of b : ", $b , "<br/>";
+    echo "Addition : " , $a + $b , "<br/>";
+    echo "Subtraction : " , $a - $b , "<br/>";
+    echo "Multiplication : " , $a * $b , "<br/>";
+    echo "Division : " , $a / $b , "<br/>";
+?>
