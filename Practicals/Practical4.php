a1e71344842b785eeb94901965525da6f3f64d71
devmulkalwar <devmulkalwar95@gmail.com>
2024-12-31 19:46:37 +0530

    practical 4 added

---
+++ Practicals/Practical4.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php
+    $a = 12.5;
+    echo "The Float Value : $a <br>";
+    $k = (int)$a;
+    echo "Converting into int Value : $k <br>";
+    $k = (string)$a;
+    echo "Converting into String Value : $k <br>";
+    ?>
+</body>
+</html>
