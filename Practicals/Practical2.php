431a828142d4864394216faa7088c76b16c5c53b
devmulkalwar <devmulkalwar95@gmail.com>
2024-12-31 19:37:03 +0530

    practical 2 added

---
+++ Practicals/Practical2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>  
+<head> 
+<title>Array Values</title> 
+</head> 
+<body> 
+<?php 
+$color = array("green", "red", "yellow","blue","black"); 
+foreach ($color as $value) 
+{ 
+echo "$value <br>"; 
+} 
+?> 
+</body> 
+</html> 
